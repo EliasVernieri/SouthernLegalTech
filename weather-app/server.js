3580ff7aac93ad06be2bfe47dd09720b32161027
elias <eliasvernieri@gmail.com>
2020-08-21 10:12:35 -0300

    pdftest

--- weather-app/server.js
+++ weather-app/server.js
@@ -2,7 +2,53 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
 
- 
+
+//Required package
+const pdf = require("pdf-creator-node");
+const fs = require('fs');
+
+// Read HTML Template
+var html = fs.readFileSync('MiPDFTemplate.html', 'utf8');
+
+var options = {
+  format: "A3",
+  orientation: "portrait",
+  border: "10mm",
+  header: {
+    height: "45mm",
+    contents: '<div style="text-align: center;">Author: Shyam Hajare</div>'
+  },
+  "footer": {
+    "height": "28mm",
+    "contents": {
+      first: 'Cover page',
+      2: 'Second page', // Any page number is working. 1-based index
+      default: '<span style="color: #444;">{{page}}</span>/<span>{{pages}}</span>', // fallback value
+      last: 'Last Page'
+    }
+  }
+};
+var users = [
+  {
+    name: "Shyam",
+    age: "26"
+  },
+  {
+    name: "Navjot",
+    age: "26"
+  },
+  {
+    name: "Vitthal",
+    age: "26"
+  }
+];
+var document = {
+  html: html,
+  data: {
+    users: users
+  },
+  path: "./output.pdf"
+}
 
 
 
@@ -18,9 +64,19 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   res.render('index');
   console.log(req.body.doc);
- }) 
+  
+  pdf.create(document, options)
+  .then(res => {
+      console.log(res)
+  })
+  .catch(error => {
+      console.error(error)
+  });
+});
 
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!')
 })
+
+
